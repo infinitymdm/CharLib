049c223167341cb8ce678e7312cae1107f4a0f46
Marcus Mellor <infinitymdm@gmail.com>
2025-07-29 11:39:21 -0500

    tests: Use gf180mcu_osu_sc's included charlib.yml file

---
+++ test/pdks/gf180mcu_osu_sc/test_gf180mcu_osu_sc.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+rm -rf gf180mcu_osu_sc_temp
+
+set -e
+
+# Clone the pdk
+git clone https://github.com/stineje/globalfoundries-pdk-libs-gf180mcu_osu_sc.git gf180mcu_osu_sc_temp
+pushd gf180mcu_osu_sc_temp
+
+# Fetch models from the primitives repo
+mkdir models
+curl https://raw.githubusercontent.com/fossi-foundation/globalfoundries-pdk-libs-gf180mcu_fd_pr/refs/heads/main/models/ngspice/sm141064.ngspice > models/sm141064.ngspice
+curl https://raw.githubusercontent.com/fossi-foundation/globalfoundries-pdk-libs-gf180mcu_fd_pr/refs/heads/main/models/ngspice/design.ngspice > models/design.ngspice
+
+# 9-track
+cd gf180mcu_osu_sc_gp9t3v3
+charlib run .
+
+# TODO: 12-track
+
+popd
