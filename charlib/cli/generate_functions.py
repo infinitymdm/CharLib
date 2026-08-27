349bf1097f7eff38f260eaa93d2d265067145dc3
Marcus Mellor <infinitymdm@gmail.com>
2026-08-27 13:06:12 -0500

    cli: Remove deprecated subcommands

--- charlib/cli/generate_functions.py
+++
@@ -1,5 +0,0 @@
-from charlib.characterizer.logic.functions import generate_yml
-
-def generate_functions(args):
-    """Generate YAML files for caching function-to-test-vector mappings"""
-    generate_yml(args.expressions)
--- charlib/cli/main.py
+++ charlib/cli/main.py
@@ -2,64 +2,48 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-from pathlib import Path
 
-from charlib.cli import run, compare
+from charlib.cli import run
 
+
+# TODO: consider using click or typer in place of argparse
 def main():
     """Run CharLib CLI"""
     # Set up charlib arguments
-    parser = argparse.ArgumentParser(
-        prog='charlib',
-        description='Standard cell library characterizer')
-    parser.add_argument(
-        '--debug', action='store_true',
-        help='Dump extra information to debug_dir')
-    parser.add_argument(
-        '-q', '--quiet', action='store_true',
-        help='Reduce the amount of information displayed')
+    parser = argparse.ArgumentParser(prog="charlib", description="Standard cell library characterizer")
+    parser.add_argument("--debug", action="store_true", help="Dump extra information to debug_dir")
+    parser.add_argument("-q", "--quiet", action="store_true", help="Reduce the amount of information displayed")
 
-    # Set up run, compare, and generate_functions subcommands
-    subparser = parser.add_subparsers(title='subcomamands', required=True)
-    parser_characterize = subparser.add_parser('run', help='Characterize a standard cell library')
-    parser_compare = subparser.add_parser(
-        'compare',
-        help='(experimental) Compare two liberty files')
+    # Set up run subcommand
+    # Other subcommands may be added in the future
+    subparser = parser.add_subparsers(title="subcomamands", required=True)
+    parser_characterize = subparser.add_parser("run", help="Characterize a standard cell library")
 
     # Set up charlib run arguments
     parser_characterize.add_argument(
-        'library', type=str,
-        help='The directory containing the library characterization configuration file, or the full path to the file')
+        "library",
+        type=str,
+        help="A directory containing a valid config YAML file, or the full path such a file",
+    )
     parser_characterize.add_argument(
-        '-o', '--output', type=str, default='',
-        help='Place the characterization results in the specified file')
+        "-o", "--output", type=str, default="", help="Place the characterization results in the specified file"
+    )
+    parser_characterize.add_argument("-j", "--jobs", type=int, default=0, help="Specify the number of concurrent jobs")
     parser_characterize.add_argument(
-        '-j', '--jobs', type=int, default=0,
-        help='Specify the number of concurrent jobs')
+        "-n", "--no-sim", action="store_true", help="Perform all tasks except for running simulations"
+    )
     parser_characterize.add_argument(
-        '-n', '--no-sim', action='store_true',
-        help='Perform all tasks except for running simulations')
-    parser_characterize.add_argument(
-        '-f', '--filters', nargs='*',
-        help='A list of one or more regex strings. charlib will only characterize cells matching one or more of the filters.')
+        "-f",
+        "--filters",
+        nargs="*",
+        help="A list of one or more regex strings. Cell names matching one of the filters will be characterized",
+    )
     parser_characterize.set_defaults(func=run.run)
-
-    # Set up charlib compare arguments
-    def compare_helper(args):
-        """Helper function for compare subcommand"""
-        with open(Path(args.compared), 'r') as compared:
-            compare.compare(args.benchmark, compared.read())
-    parser_compare.add_argument(
-        'benchmark',  type=str,
-        help='A liberty file to use as a benchmark for comparison')
-    parser_compare.add_argument(
-        'compared', type=str,
-        help='A liberty file to compare against the benchmark')
-    parser_compare.set_defaults(func=compare_helper)
 
     # Parse args and execute
     args = parser.parse_args()
     args.func(args)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
