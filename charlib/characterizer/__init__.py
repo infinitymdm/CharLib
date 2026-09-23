9a5797b77b31e20f9b6d5ff43eb4396aed2fdcc7
Marcus Mellor <infinitymdm@gmail.com>
2026-09-23 14:03:31 -0500

    characterizer: Add docstrings to CharacterizationWorkChain outline

--- charlib/characterizer/__init__.py
+++ charlib/characterizer/__init__.py
@@ -8,13 +8,14 @@ class CharacterizerWorkChain(WorkChain):
         super().define(spec)
 
         spec.input(
-            "config_file",
+            "config",
             valid_type=SinglefileData,
             help="A CharLib configuration YAML file containing characterization settings and cell configurations",
         )
         spec.outline(
             cls.load_config,
-            cls.run_measurements,
+            cls.map_procedures,
+            cls.run_procedures,
             cls.merge_results,
         )
         spec.output(
@@ -22,10 +23,17 @@ class CharacterizerWorkChain(WorkChain):
         )
 
     def load_config(self):
+        """Read the config file and create or load nodes from the database"""
         pass  # TODO
 
-    def run_measurements(self):
+    def map_procedures(self):
+        """Iterate over cells in the config and identify which procedures we can run with the given parameters"""
+        pass  # TODO
+
+    def run_procedures(self):
+        """Run previously mapped procedures with as much parallelism as possible"""
         pass  # TODO
 
     def merge_results(self):
+        """Collect all procedures' results and merge them into a liberty file for the whole cell library"""
         pass  # TODO
